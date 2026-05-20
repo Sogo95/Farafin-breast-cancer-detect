f5bf468c43413898609ba0ac026f2362f33143dd
Sogo Armel Emmanuel <140093155+Sogo95@users.noreply.github.com>
2026-05-20 14:59:05 +0000

    last

--- apprcde.py
+++ apprcde.py
@@ -357,9 +357,8 @@ def login_page():
             box-shadow: 0 12px 40px rgba(236,72,153,0.45);
             margin-bottom: 1.5rem;
         ">
-            <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402z" fill="white"/>
-            </svg>
+            <img src='https://img.icons8.com/color/240/pink-ribbon.png' width='160'>
+
         </div>
         <h1 style="font-family:'Playfair Display',serif; font-size:1.75rem; font-weight:700; color:white; margin:0 0 8px;">
             Farafin BreastCancer AI Detect
