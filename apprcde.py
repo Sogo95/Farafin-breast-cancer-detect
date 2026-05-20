9af3c725dba9f764d301041fd407c8b705eb5387
Sogo Armel Emmanuel <140093155+Sogo95@users.noreply.github.com>
2026-05-20 14:53:35 +0000

    last

--- apprcde.py
+++ apprcde.py
@@ -347,30 +347,26 @@ def login_page():
     # LOGO + TITRE
     st.markdown("""
     <div style="text-align:center; margin-bottom: 2.5rem;">
-
         <div style="
             display: inline-flex;
             align-items: center;
             justify-content: center;
-            width: 90px;
-            height: 90px;
+            width: 80px; height: 80px;
             border-radius: 22px;
-            background: white;
-            box-shadow: 0 12px 40px rgba(15,31,61,0.15);
+            background: linear-gradient(135deg, #ec4899, #a855f7);
+            box-shadow: 0 12px 40px rgba(236,72,153,0.45);
             margin-bottom: 1.5rem;
-            overflow: hidden;
         ">
-            <img src="https://www.google.com/imgres?q=octobre%20rose&imgurl=https%3A%2F%2Fmaginfrance.fr%2Fwp-content%2Fuploads%2F2019%2F10%2Fimage-octobre-rose-e1570609381510-750x437.jpg&imgrefurl=https%3A%2F%2Fmaginfrance.fr%2Foctobre-rose-quand-beaute-rime-avec-solidarite%2F&docid=XCqnYO80pg6zcM&tbnid=8YIi-v3fSWZmwM&w=750&h=437&hcb=2" style="width: 70px; height: 70px; object-fit: contain;">
+            <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402z" fill="white"/>
+            </svg>
         </div>
-
         <h1 style="font-family:'Playfair Display',serif; font-size:1.75rem; font-weight:700; color:white; margin:0 0 8px;">
             Farafin BreastCancer AI Detect
         </h1>
-
         <p style="color:#94b3c8; font-size:0.9rem; margin:0; font-weight:300; letter-spacing:0.05em;">
             Système d'aide au diagnostic du cancer du Sein
         </p>
-
     </div>
     """, unsafe_allow_html=True)
 
